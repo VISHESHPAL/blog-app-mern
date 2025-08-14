19e8b85ed0ae0a025e0ea7339a35e48c41956bc9
VISHESHPAL <156537104+VISHESHPAL@users.noreply.github.com>
2025-08-14 16:57:13 +0530

    setup the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+
+   <h1 className='text-center text-5xl mt-3'>BLOG APP</h1>
+   </>
+  )
+}
+
+export default App
