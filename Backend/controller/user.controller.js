e130adabdca5d5c38de63452007562262e5246c8
VISHESHPAL <156537104+VISHESHPAL@users.noreply.github.com>
2025-08-16 13:08:27 +0530

    user login register controller completed and the routes are working properly

---
+++ Backend/controller/user.controller.js
@@ -0,0 +1,102 @@
+import User from "../model/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(401).json({
+        success: false,
+        message: "All Fields are Required ",
+      });
+    }
+
+    const existedUser = await User.findOne({ email });
+
+    if (existedUser) {
+      return res.status(400).json({
+        success: false,
+        message: " User Already Exist ! ",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "User Registered Successfully ! ",
+      user,
+    });
+  } catch (error) {
+    // console.log(error)
+    return res.status(500).json({
+      success: false,
+      message: "Error Registering the User",
+      error: error.message,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(401).json({
+        success: false,
+        message: "All fields are required ! ",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid User Credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Password Credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV,
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+
+    return res.status(201).json({
+      success: true,
+      message: "User Login Successfully ! ",
+      user,
+      token,
+    });
+
+    
+  } catch (error) {
+    // console.log(error)
+    return res.status(500).json({
+      success: false,
+      message: "Error Logging  the User",
+      error: error.message,
+    });
+  }
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './db/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import userRouter from './routes/user.route.js';
+
 dotenv.config();
 
 const app =  express();
+app.use(cookieParser());
+app.use(express.json())
+app.use(cors())
 
+
+app.use("/api/user" , userRouter)
 
 app.get("/" , (req, res) =>{
      res.send("BACKEND IS WORKING ")
